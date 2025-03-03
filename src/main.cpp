6989130144bb8dd716d7dca991d85c6dce4f9d10
D1ZERLEO <levarazorenov@mail.ru>
2025-03-03 14:41:58 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <utils.cpp>
+#include <iostream>
+
+int main() {
+	int a = 5;
+	int b = 4;
+	std::cout << "ADDITION" << " " << addition(a, b) << std::endl;
+	std::cout << "SUBSTRACTION" << " " << substraction(a, b) << std::endl;
+	std::cout << "DIVISION" << " " << division(a, b) << std::endl;
+	std::cout << "EXPONENTATION" << " " << exponentiation(a, b) << std::endl;
+}
---
+++ src/utils.cpp
@@ -0,0 +1,28 @@
+inline int addition(int x, int y) {
+	return x + y;
+}
+
+inline int substraction(int x, int y) {
+	return x - y;
+}
+
+inline int division(int x, int y) {
+	return x / y;
+}
+
+
+inline int exponentiation(int x, int y) {
+	if (y == 0) {
+		return 1;
+	}
+	else {
+		int a = 1;
+		while (y != 0) {
+			a *= x;
+			y -= 1;
+		}
+		return a;
+	}
+
+
+}
